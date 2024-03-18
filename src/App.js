616274bf8ec9f639d553aceeed34b491805a0c37
James Clifford <jamesclifford7@gmail.com>
2024-03-18 16:23:52 -0700

    wrap app.js in userprovider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
 import AppRoutes from './components/Routes';
+import UserProvider from './components/UserProvider';
 
 function App() {
   return (
     <div className="App">
-      <AppRoutes />
+      <UserProvider>
+        <AppRoutes />
+      </UserProvider>
     </div>
   );
 }
