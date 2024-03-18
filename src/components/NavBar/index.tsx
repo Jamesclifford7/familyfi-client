ca6ed591df936846128b1bf4935db14ba5b71712
James Clifford <jamesclifford7@gmail.com>
2024-03-18 14:28:44 -0700

    add mobile nav

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -14,27 +14,60 @@ export default function NavBar() {
     }
 
     return (
-        <StyledNav>
-            <LogoContainer>
-                <Logo>FamilyFi</Logo>
-                <PiggyBank src={piggyBankIcon} />
-            </LogoContainer>
-            <LinkContainer>
-                <StyledLink to="/account_overview">Account Overview</StyledLink>
-                <StyledLink to="/marketplace">Marketplace</StyledLink>
-                <LogoutButton onClick={(e) => handleLogout(e)}>Logout</LogoutButton>
-            </LinkContainer>
-        </StyledNav>
+        <>
+            <DesktopNav>
+                <LogoContainer>
+                    <Logo>FamilyFi</Logo>
+                    <PiggyBank src={piggyBankIcon} />
+                </LogoContainer>
+                <LinkContainer>
+                    <StyledLink to="/account_overview">Account Overview</StyledLink>
+                    <StyledLink to="/marketplace">Marketplace</StyledLink>
+                    <LogoutButton onClick={(e) => handleLogout(e)}>Logout</LogoutButton>
+                </LinkContainer>
+            </DesktopNav>
+            <MobileNavContainer>
+                <MobileNav>
+                    <StyledLink to="/account_overview">Overview</StyledLink>
+                    <StyledLink to="/marketplace">Marketplace</StyledLink>
+                    <LogoutButton onClick={(e) => handleLogout(e)}>Logout</LogoutButton>
+                </MobileNav>
+            </MobileNavContainer>
+        </>
     )
 }
 
-const StyledNav = styled.nav`
+const DesktopNav = styled.nav`
     border-bottom: solid #000000; 
     background-color: #64F58D; 
-    display: flex; 
+    display: none; 
     margin: 0; 
     padding: 15px 0; 
     width: 100%; 
+    justify-content: space-between; 
+
+    @media all and (min-width: 415px) {
+        display: flex; 
+    }
+`
+
+const MobileNavContainer = styled.div`
+    display: inherit; 
+    position: fixed !important; 
+    width: 100%; 
+    bottom: 0; 
+
+    @media all and (min-width: 415px) {
+        display: none; 
+    }
+`
+
+const MobileNav = styled.nav`
+    border-top: solid 0.5px #000000; 
+    background-color: #64F58D; 
+    display: flex; 
+    flex-direction: row; 
+    padding: 25px 20px; 
     justify-content: space-between; 
 `
 
@@ -64,11 +97,14 @@ const LinkContainer = styled.div`
 const StyledLink = styled(Link)`
     text-decoration: none; 
     font-size: 16px; 
-    margin-right: 35px; 
     color: #000000; 
 
     &:hover {
         text-decoration: underline; 
+    }
+
+    @media all and (min-width: 415px) {
+        margin-right: 35px; 
     }
 `
 
@@ -76,11 +112,14 @@ const LogoutButton = styled.button`
     background: none; 
     border: none; 
     font-size: 16px; 
-    margin-right: 35px; 
     color: #000000; 
 
     &:hover {
         text-decoration: underline; 
         cursor: pointer; 
+    }
+
+    @media all and (min-width: 415px) {
+        margin-right: 35px; 
     }
 `
